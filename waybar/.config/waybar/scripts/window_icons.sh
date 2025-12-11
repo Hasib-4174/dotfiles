71f8f264e1de1c988582c99678e27ebd42f024a3
Hasib-4174 <hasibkhan4174@gmail.com>
2025-12-11 19:44:23 +0600

    add configs

--- waybar/.config/waybar/scripts/window_icons.sh
+++ waybar/.config/waybar/scripts/window_icons.sh
@@ -2,23 +2,27 @@
 
 # Map app names to icons (edit this as you add more apps)
 declare -A icons=(
-  ["Brave-browser"]="󰈹"
-  ["firefox"]=""
-  ["Alacritty"]=""
-  ["foot"]=""
-  ["kitty"]=""
-  ["Code"]="󰨞"
-  ["GitKraken"]=""
-  ["org.kde.dolphin"]=""
-  ["pcmanfm"]=""
-  ["vlc"]=""
-  ["mpv"]=""
-  ["discord"]=""
-  ["steam"]=""
-  ["spotify"]=""
+  ["brave-browser"]=" 󰈹"
+  ["firefox-developer-edition"]=" "
+  ["firefox"]=" "
+  ["google-chrome"]=" "
+  ["Alacritty"]=" "
+  ["foot"]=" "
+  ["kitty"]=" "
+  ["Code"]=" 󰨞"
+  ["GitKraken"]=" "
+  ["org.kde.dolphin"]=" "
+  ["pcmanfm"]=" "
+  ["vlc"]=" "
+  ["mpv"]=" "
+  ["discord"]=" "
+  ["steam"]=" "
+  ["spotify"]=" "
+  ["jetbrains-studio"]=" "
 )
 
 # Get all visible windows in the current workspace
+#hyprctl clients -j | jq -r '.[] | .class'
 hyprctl clients -j | jq -r '.[] | select(.workspace.id != -1 and .workspace.id == .workspace.id) | .class' |
   awk '!seen[$0]++' |
   while read -r app; do
